d11235bcd0e19b0a439f1ca7af51a09a69d05a08
(faizsltn) <(faizsulthondaudm@gmail.com)>
2025-05-06 10:37:07 +0700

    membuat prosedur

--- contoh.cpp
+++ contoh.cpp
@@ -7,4 +7,19 @@ class PersegiPanjang
     int panjang, lebar;
 
     public:
-}
+
+    void input()
+    {
+        cout << "Masukkan panjang : ";
+        cin >> panjang;
+        cout << "Masukkan lebar :";
+        cin >> lebar;
+    }
+
+    int luasPp(int a, int b)
+    {
+        return a * b;
+    }
+
+    Lingkaran ol;
+};
