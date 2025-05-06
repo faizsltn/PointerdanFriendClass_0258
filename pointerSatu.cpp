148cd050f82f721f86c84c1ad652141ea6111793
(faizsltn) <(faizsulthondaudm@gmail.com)>
2025-05-06 11:04:09 +0700

    membuat object mhs , pointer reference dan deference

--- pointerSatu.cpp
+++ pointerSatu.cpp
@@ -10,3 +10,18 @@ class mahasiswa
         cout << "No Induk = " << nim << endl;
     }
 };
+
+int main()
+{
+    mahasiswa mhs{1}; // object mhs
+    mhs.showNim(); //member acces operator
+
+    mahasiswa &refMhs = mhs; // pointer reference refMhs
+    refMhs.nim = 2; // member acces operator
+    mhs.showNim();
+
+    mahasiswa *pMhs = &mhs; // pointer deference pMhs
+    pMhs->nim = 3; //arrow operator
+    pMhs->showNim();
+    return 0;
+}
